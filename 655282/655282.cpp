422c4ed3f0146d02fc9fab38cb995bffce01136a
paulchien223 <55919406+paulchien223@users.noreply.github.com>
2020-04-16 08:15:19 +0800

    Create 655282.cpp

---
+++ 655282/655282.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int count;
+  cin >> count;
+
+  int nums[count];
+  for (int i = 0; i < count; i++) {
+    cin >> nums[i];
+  }
+
+  for (int i = 0; i < count; i++) {
+    int low = nums[i];
+    if (nums[i] < low) {
+        low = nums[i];
+      }
+  }
+  
+  for (int j = count; j > 0; j--) {
+    for (int i = 0; i < j; i++) {
+      if (nums[i] > nums[i + 1]) {
+        int save = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = save;
+      }
+    }
+  }
+
+  for (int i = 0; i < count; i++) {
+    cout << nums[i] << "\n";
+  }
+}
